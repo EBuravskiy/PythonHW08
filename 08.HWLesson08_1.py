d64152737894b2f09cbc4b5e233a98be7a95f425
EBuravskiy <eburavskiy81@mail.ru>
2023-07-10 09:31:40 +0500

    Update 08.HWLesson08_1.py

--- 08.HWLesson08_1.py
+++ 08.HWLesson08_1.py
@@ -1,17 +1,24 @@
-print("Enter the number of list elements:")
+print("Enter the number of list elements from 1 to 10000:")
 number = int(input())
-list1 = []
-print("Enter elements of the list from 1 to 10000:")
-for i in range(number):
-    element = int(input())
-    if element < 1:
+if number < 1:
         print("You entered a number less than 1, which does not match the condition. Please try again.")
         quit()
-    elif element > 10000:
+elif number > 10_000:
         print("You entered a number greater than 10000. Please try again.")
         quit()
-    else:
-        list1.append(element)
+else:     
+    list1 = []
+    print("Enter elements of the list from -1000000 to 1000000:")
+    for i in range(number):
+        element = int(input())
+        if element < -1_000_000:
+            print("You entered a number less than 1000000, which does not match the condition. Please try again.")
+            quit()
+        elif element > 1_000_000:
+            print("You entered a number greater than 1000000. Please try again.")
+            quit()
+        else:
+            list1.append(element)
 print("Elements entered in the list:")
 print(list1)
 list1.reverse()
